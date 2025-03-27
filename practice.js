30d6c011cb563f23a2f2f4807f41ba1b1388cb64
danforsomereason <147667936+danforsomereason@users.noreply.github.com>
2025-03-27 11:25:57 -0400

    Completed project per instructions

--- practice.js
+++
@@ -1,8 +0,0 @@
-for(let i = 0; i < 5; i++) {
-    let row = ""
-    for(let j = 0; j < 5; j++) {
-        row += '#';
-    }
-    console.log(row);
-    
-}
--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 let size = 64;
-let squareWidth = (size) => {
-    let squares = size * size;
-    console.log("Number of squares: ", squares);
-    let squareArea = Math.floor(160000 / squares);
-    console.log("Area of each square: ", squareArea);
-    let squareWidth = Math.floor(Math.sqrt(squareArea));
-    console.log("Width of each square: ", squareWidth);
-    return squareWidth;
-}
 
 // create container
 const container = document.createElement('div');
 container.classList.add("container");
 container.setAttribute("style",
-    `outline: 1px solid lime; display: flex; flex-wrap: wrap; width: 400px; height: 400px; margin: auto;`
+    `border: 1px solid black; display: flex; flex-wrap: wrap; width: 400px; height: 400px; margin: auto;`
 )
 document.body.prepend(container);
 
 
 // create grid of divs
 function generateSquares(size){
-    const side = squareWidth(size);
+    container.replaceChildren();
     for(let i = 0; i < size; i++) {
         const row = document.createElement('div');
         row.classList.add("row");
@@ -29,11 +20,14 @@ function generateSquares(size){
         for(let j = 0; j < size; j++) {
             const square = document.createElement('div');
             // square.classList.toggle("square");
-            square.setAttribute("style", `outline: 1px solid black; margin: 0; height: ${side}px; width: ${side}px;`);
+            square.setAttribute("style", `margin: 0; width: 100%;`);
             row.appendChild(square);
 
-            square.addEventListener("mousemove", ()=> {
-                square.classList.add("filled");
+            square.addEventListener("mouseover", (e) => {
+                let randomColor = Math.floor(Math.random() * 360);
+                let random100 = Math.floor(Math.random() * 101);
+                console.log("Target: ", e.target)
+                e.target.style.background = `hsl(${randomColor}deg, ${random100}%, ${random100}% )`;
             })
         }
     }
@@ -53,13 +47,11 @@ changeSizeBtn.addEventListener('click', ()=> {
     size = prompt("Enter number of squares per side (must be between 2 and 100)");
     if(size === null || size < 2) {
         alert("You must enter a valid number");
-    } else if (size > 99){
+    } else if (size > 100){
         alert("You must enter a number between 2 and 100")
     } else {
-        container.remove();
-        createContainer();
+        console.log("Result of listener: ", size);
         generateSquares(size);
-        console.log("Result of listener: ", size, squareWidth);
     }
 })
 
